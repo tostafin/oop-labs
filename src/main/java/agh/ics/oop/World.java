f36348195b7f3da464babc716589d18780cef26f
Tomasz Ostafin <ostafintomasz1@gmail.com>
2021-10-21 16:07:09 +0200

    Second lab

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -48,7 +48,12 @@ public class World {
             }
         }
         System.out.println("Start");
-        run(directions);
+//        run(directions);
+        Vector2d position1 = new Vector2d(1,2);
+        System.out.println(position1);
+        Vector2d position2 = new Vector2d(-2,1);
+        System.out.println(position2);
+        System.out.println(position1.add(position2));
         System.out.println("Stop");
     }
 }
