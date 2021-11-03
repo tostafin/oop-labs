95059f00b4655bca490b39065e18c131b89b62ea
Tomasz Ostafin <ostafintomasz1@gmail.com>
2021-11-03 22:13:47 +0100

    lab3 finished – little change in World ver. 3

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -77,7 +77,7 @@ public class World {
         Do 10.
         1) Tablica dwuwymiarowa typu boolean
         2) Trzymanie zwierząt w liście
-        Jeśli zwierząt jest mało, to lista będzie lepsza. W przeciwnym przypadku tablica będzie lepsza.
+        Jeśli zwierząt jest mało, to lista będzie lepsza. W przeciwnym wypadku tablica będzie lepsza.
         */
     }
 }
