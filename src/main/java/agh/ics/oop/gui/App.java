9aa311b5117c5b0c1d56bce908ee30b6551f37cf
Tomasz Ostafin <ostafintomasz1@gmail.com>
2021-12-08 17:21:55 +0100

    lab 7a – final version

--- src/main/java/agh/ics/oop/gui/App.java
+++ src/main/java/agh/ics/oop/gui/App.java
@@ -10,81 +10,57 @@ import javafx.scene.layout.RowConstraints;
 import javafx.stage.Stage;
 import agh.ics.oop.*;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Vector;
-import java.util.stream.Collectors;
-
 public class App extends Application {
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) {
         String[] args = getParameters().getRaw().toArray(new String[0]);
         MoveDirection[] directions = new OptionsParser().parse(args);
         GrassField map = new GrassField(10);
         Vector2d[] positions = {new Vector2d(2, 2), new Vector2d(3, 4)};
         IEngine engine = new SimulationEngine(directions, map, positions);
         engine.run();
-        int[] lowerLeft = {map.getLowerLeft().x, map.getLowerLeft().y};
-        int[] upperRight = {map.getUpperRight().x, map.getUpperRight().y};
-        List<Grass> fields = map.fields;
-        List<Vector2d> v = fields.stream().map(Grass::getPosition).collect(Collectors.toList());
-        System.out.println(v);
-        System.out.println(Arrays.toString(lowerLeft));
-        System.out.println(Arrays.toString(upperRight));
+        Vector2d lowerLeft = map.getLowerLeft();
+        Vector2d upperRight = map.getUpperRight();
 
-//        int[] lowerLeft = {-5, 1};
-        int[] lowerLeftCopy = lowerLeft.clone();
-//        int[] upperRight = {8, 9};
-        int[] upperRightCopy = upperRight.clone();
-        int y = upperRight[1] - lowerLeft[1] + 1;
-        int x = upperRight[0] - lowerLeft[0] + 1;
+        int height = upperRight.y - lowerLeft.y + 1;
+        int width = upperRight.x - lowerLeft.x + 1;
         primaryStage.setTitle("World Map");
         GridPane gridPane = new GridPane();
         gridPane.setGridLinesVisible(true);
 
-        for (int i = 0; i <= x; i++) {
-            ColumnConstraints columnConstraints = new ColumnConstraints(50);
-            columnConstraints.setPercentWidth(100.0 / x);
+        for (int i = 0; i <= width; i++) {
+            ColumnConstraints columnConstraints = new ColumnConstraints(20);
+            columnConstraints.setPercentWidth(100.0 / width);
             gridPane.getColumnConstraints().add(columnConstraints);
         }
 
-        for (int i = 0; i <= y; i++) {
-            RowConstraints rowConstraints = new RowConstraints();
-            rowConstraints.setPercentHeight(100.0 / y);
+        for (int i = 0; i <= height; i++) {
+            RowConstraints rowConstraints = new RowConstraints(20);
+            rowConstraints.setPercentHeight(100.0 / height);
             gridPane.getRowConstraints().add(rowConstraints);
         }
-        int xDiff = lowerLeft[0] - 1;
-        int yDiff = upperRight[1] + 3;
-        for (int i = 0; i <= y; i++) {  // i – row
+
+        int xDiff = lowerLeft.x - 1;
+        int yDiff = upperRight.y + 3;
+        for (int i = 0; i <= height; i++) {
             yDiff -= 2;
-            for (int j = 0; j <= x; j++) {  // j - column
-                int y1 = j, x1 = i;
-                String text = null;
-                if (i == 0 && j == 0) text = "y / x";
-                else if (i == 0) {
-                    text = String.valueOf(lowerLeftCopy[0]);
-                    lowerLeftCopy[0]++;
-                }
-                else if (j == 0) {
-                    text = String.valueOf(upperRightCopy[1]);
-                    upperRightCopy[1]--;
-                }
+            for (int j = 0; j <= width; j++) {
+                String character = null;
+                if (i == 0 && j == 0) character = "y/x";
+                else if (i == 0) character = String.valueOf(lowerLeft.x + j - 1);
+                else if (j == 0) character = String.valueOf(upperRight.y - i + 1);
                 else {
                     Object obj = map.objectAt(new Vector2d(j + xDiff, i + yDiff));
-                    if (obj != null) {
-                        text = obj.toString();
-                        System.out.println(obj);
-                        System.out.println(i + " " + j);
-                    }
+                    if (obj != null) character = obj.toString();
                 }
-                Label label = new Label(text);
-                GridPane.setConstraints(label, y1, x1);
+                Label label = new Label(character);
+                GridPane.setConstraints(label, j, i);
                 GridPane.setHalignment(label, HPos.CENTER);
-                gridPane.add(label, y1, x1);
+                gridPane.add(label, j, i);
             }
         }
 
-        Scene scene = new Scene(gridPane, 50 * x, 50 * y);
+        Scene scene = new Scene(gridPane, 50 * width, 50 * height);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
@@ -92,8 +68,4 @@ public class App extends Application {
     public static void main(String[] args) {
         Application.launch(App.class, args);
     }
-    /*
-    * Do testowania:
-    * f b r l f f r r f f f f f f f f
-    * l l f f f f f f f f f f f f f f*/
 }
